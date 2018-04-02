7f13d4437597b7f5f8f9836ac3d7f30c4d1d4a93
Alfredo Reyes <reyes-fred@hotmail.com>
2018-04-02 14:38:01 -0700

    Update

--- Scripts/Autonomous/simulator/start.sh
+++ Scripts/Autonomous/simulator/start.sh
@@ -1,21 +1,30 @@
+set -x
 echo
 echo "Red balloon simulator"
 echo
 
 export path=/home/alfredo/Desktop/Xiaomin/Scripts/Autonomous/scripts
 export qground=/home/alfredo/Downloads/
+export sour=/home/alfredo/Desktop/Xiaomin/Setup/
+
+workon cv 
+
+#Execute source
+cd $sour
+source config.sh
 
 #Execute mavlink
 cd $path
 ./linux_run_strategy.sh &
-
 #Wait until mavproxy launch
 
 #Execute QGroundControl
 cd $qground
-(sleep 10 && ./QGroundControl.AppImage)&
+sleep 10
+./QGroundControl.AppImage &
 
+sleep 10
 #Execute Python Ballon
 cd $path
 python balloon_strategy.py &
-
+sleep 10
--- Setup/config.sh
+++ Setup/config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export camera=1 #1 -  Internal web cam  and 0 for IntelAero cam
 #export connection='tcp:127.0.0.1:5607' #Simulator
-export protocol='udp' #TCP or UDP
+export protocol='tcp' #TCP or UDP
 export ip='127.0.0.1'
-export port='14551' #5706 for drone an 14550/14551 for SITL
+export port='5763' #5706 for drone an 14550/14551 for SITL
